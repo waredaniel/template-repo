86d3ad63b2cc515a60279571e92def6414ed2a51
danielware <waredanielb@gmail.com>
2022-06-27 10:50:48 -0700

    Added files.
    Co-authorized- by Zhibin Liang <ifthereisoneday@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import $ from 'jquery';
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    const zipcode = $('#zipcode').val();
+    $('#location').val("");
+    $('#zipcode').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const urlZipcode = `https://api.openweathermap.org/data/2.5/weather?q=${zipcode}&appid=${process.env.API_KEY}`
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        /* eslint-disable*/
+        getElements(response);
+        /* eslint-enable*/
+      }
+    };
+
+    request.open("GET", url, true);
+    request.open("GET",urlZipcode, true);
+    request.send();
+  
+    /* eslint-disable*/
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      let fahrenheitTemp= ((`${response.main.temp}` -273.15)*(9/5))+32
+      $('.showFahrenheit').text(`The temperature in Fahrenheit is ${fahrenheitTemp}`)
+      $('.showLon').text(`The longitude is ${response.coord.lon}`);
+      $('.showLat').text(`The latitude is ${response.coord.lat}`);
+    };
+    /* eslint-enable*/
+  });
+});
